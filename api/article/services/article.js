e53dd762b6d2c3c7961609bfbcd1d8caf161592e
Manu <manu.scazza@hotmail.it>
2021-03-15 18:33:31 +0100

    merger+

--- api/article/services/article.js
+++ api/article/services/article.js
@@ -19,6 +19,29 @@ module.exports = {
       status: 'published',
       _sort: 'publishedAt:DESC',
       ...params
+    };
+    if (category) customParams['category'] = category.toString();
+    return strapi.query('article').find(customParams, populate);
+  },
+
+  async count(params, populate) {
+    let category;
+    if (params?.hasOwnProperty('category_name')) {
+      const el = await strapi.query('category').findOne({ slug: params['category_name'] }, populate);
+      category = el?._id;
+      delete params['category_name'];
+    }
+    const customParams = {
+      ...params
+    };
+    if (category) customParams['category'] = category.toString();
+    return strapi.query('article').count(customParams);
+  },
+
+  findAllSlugs(params, populate) {
+    const customParams = {
+      status: 'published',
+      _limit: '-1'
     }
     return strapi.query('article').find(customParams, populate);
   },
